fda5a2f0d09c0fa00fcca2b0c1f39dc5d7d583b9
inuter <inuter@qq.com>
2020-09-10 22:20:09 +0800

    initial commit

---
+++ src/test/java/tk/inuter/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package tk.inuter.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
